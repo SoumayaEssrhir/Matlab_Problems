21f7abd71077e164af23920d55e8ce58905cd3dc
Soumaya Essrhir <113376608+SoumayaEssrihr@users.noreply.github.com>
2024-03-08 12:55:49 +0100

    Autograder.m

--- Hundle functions/Autograder.m
+++ Hundle functions/Autograder.m
@@ -1,1 +1,13 @@
+function result = grader(func_student, func_reference, varargin)
+
+    result = true; 
+    for i = 1:numel(varargin)
+        output_student = func_student(varargin{i});
+        output_reference = func_reference(varargin{i});
+        if ~isequal(output_student, output_reference)
+            result = false; 
+            break; 
+        end
+    end
+end
 
