4a51ddf31bc6542495ea7f8d38907261aa4f68d9
Soumaya Essrhir <113376608+SoumayaEssrihr@users.noreply.github.com>
2024-03-08 13:00:07 +0100

    Polynomial.m

--- Nested functions/Polynomial.m
+++ Nested functions/Polynomial.m
@@ -1,6 +1,5 @@
 function fh = get_polynomial_handle(p)
     function polynomial = poly(x)
-        % Calculate the polynomial using vectorized operations
         polynomial = sum(p .* x.^(0:length(p)-1));
     end    
     fh = @poly;
